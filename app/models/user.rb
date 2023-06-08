80bf004914c7b5a7a594addd3874fe4ae322e010
chicorycoffee <yk.nonogaki@gmail.com>
2023-06-08 18:02:17 +0900

    relationshipモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_one :profile, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
+
 end
