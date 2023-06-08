81abad4166a3f9fefe174beda7b7780ec87bf9f8
chicorycoffee <yk.nonogaki@gmail.com>
2023-06-08 18:33:54 +0900

    delteアクションの追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,5 +5,10 @@ class RelationshipsController < ApplicationController
     redirect_to "/users/#{params[:user_id]}/profiles"
   end
 
-  
+  def destroy
+    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follow.destroy
+    redirect_to "/users/#{params[:user_id]}/profiles"
+  end
+
 end
